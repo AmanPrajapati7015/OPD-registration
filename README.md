4e564a8ae91b82fe36ed6c0d3b425e790fd41d79
Aman Prajapati <aman.prajapati7022@gmail.com>
2024-10-12 21:19:58 +0530

    basic file structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# OPD registration
+
+
+design doc: https://excalidraw.com/#room=f163922d443b1d826fe2,kKkb18pESz4W6Yh5AV1Ngw
+
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>Start fresh</>
+  )
+}
+
+export default App
