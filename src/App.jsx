92beb684b864bb174b453c22c710cd93171de884
Aman Prajapati <aman.prajapati7022@gmail.com>
2024-10-30 15:03:11 +0530

    add hoddashboard to routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { Signup } from './Components/LoginPage/Signup'
 import { Signin } from './Components/LoginPage/Signin'
 import { Landingpage } from './Components/DashboardPage/Patient/routes/dashboard'
 import { PatientDashboard } from './Components/DashboardPage/Patient/routes/patientdashboard'
+import AdminDashboard from './Pages/DashboardPage/Admin/Admin'
+import HODd from './Pages/DashboardPage/HOD/HODd'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 function App() {
 
@@ -17,8 +19,11 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup/>} />
           <Route path="/signin" element={<Signin />} />
-          <Route path="/landing" element={<Landingpage/>} />
+          <Route path="/" element={<Landingpage/>} />
           <Route path="/PatientDashboard" element={ <PatientDashboard/>}/>
+          <Route path="/HODDashboard" element={ <HODd/>}/>
+
+
         </Routes>
       </BrowserRouter>
 
